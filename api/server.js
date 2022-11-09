bbb8f93a75e2a9c0e3198738799efce74b5e7a19
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-09 13:18:29 -0800

    post request created

--- api/server.js
+++ api/server.js
@@ -1,7 +1,37 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const Users = require('./users/model');
 
 const server = express();
 
+server.use(express.json());
+
+server.post('/api/users', async (req, res) => {
+    try {
+        const {name, bio} = req.body;
+        if(!name || !bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            });
+        } else {
+            const newUser = await Users.insert({name, bio});
+            res.status(201).json({
+                data: newUser
+            });
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `There was an error while saving the user to the database`
+        });
+    }
+})
+
+// server.get('/api/users', async (req, res) => {
+//     try {
+
+//     } catch(err) {
+
+//     }
+// });
 
 module.exports = server;
