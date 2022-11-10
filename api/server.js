98ac242d66c50f08ef572f010c1d79fe1f7af2c8
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-09 16:02:29 -0800

    created get requests

--- api/server.js
+++ api/server.js
@@ -29,12 +29,34 @@ server.post('/api/users', async (req, res) => {
     }
 })
 
-// server.get('/api/users', async (req, res) => {
-//     try {
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await Users.find();
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(500).json({
+            message: "The users information could not be retrieved"
+        })
+    }
+});
 
-//     } catch(err) {
-
-//     }
-// });
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await Users.findById(id);
+        if(!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            // throw new Error('error');
+            res.status(200).json(user);
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The user information could not be retrieved"
+        })
+    }
+})
 
 module.exports = server;
