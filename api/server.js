207aa4185811f458c9c45579f852a7b98235b362
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-09 16:26:01 -0800

    created delete request

--- api/server.js
+++ api/server.js
@@ -59,4 +59,22 @@ server.get('/api/users/:id', async (req, res) => {
     }
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deletedUser = await Users.remove(id);
+        if(!deletedUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(deletedUser);
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        })
+    }
+})
+
 module.exports = server;
